738a254feebd6ba799f86ce4425143aceda4284d
Michael Ilie <michaelilie@Michaels-MBP.T-mobile.com>
2018-01-04 19:28:26 -0500

    fixing port error|| allowing Heroku to chose what port this app runs on

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const bodyParser = require("body-parser")
 const app = express()
 const MongoClient = require('mongodb').MongoClient
-
+var port = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -12,7 +12,7 @@ MongoClient.connect(db.url, (err, database) => {
   // ... start the server
   if (err) return console.log(err)
   require('./routes')(app,database)
-  app.listen(3000, () => {
+  app.listen(port, () => {
     console.log('Running on http://localhost:3000/')
   })
 })
